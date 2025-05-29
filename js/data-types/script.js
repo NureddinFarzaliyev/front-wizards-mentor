e54d3c2c6b55c7cd916a1e1d6b813854f01edfbf
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-05-29 19:49:24 +0400

    js

---
+++ js/data-types/script.js
@@ -0,0 +1,13 @@
+const user = {
+    username: "Ziya",
+    isStudent: true,
+    age: 19,
+    cars: ['tesla', 'hyundai', 'prius'],
+    occupation: null,
+}
+
+// Ziya
+// true 
+// 19 
+// tesla,hyundai,prius
+// null
