ba4d86e86807232399fc6d49d12d4eb7337fcba7
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-06-22 15:36:55 +0400

    ternary

---
+++ js/anonymous-fn-and-ternary/task.js
@@ -0,0 +1,21 @@
+// Parametr olaraq təyin edilmiş adı istifadə edərək "Hello, name!" yazısı çıxaran anonim (IIFE) funksiya yazın
+
+// Aşağıdakı ifadəni ternary operator ilə yazın
+
+const age = 18;
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
+
+// Aşağıdakı ifadəni ternary operator ilə yazın
+
+if (age < 18) {
+  console.log("You are a minor.");
+} else if (age >= 65) {
+  console.log("You are a senior.");
+} else {
+  console.log("You are an adult.");
+}
