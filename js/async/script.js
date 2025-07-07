4b8c58ccd5d3371143825b8193caef0219b3d322
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-07-07 20:51:02 +0400

    async/await & interval/timeout

--- js/async/script.js
+++ js/async/script.js
@@ -2,4 +2,20 @@ const url = "https://fakestoreapi.com/products";
 
 // then chaining ilə datanı fetch edin
 
+fetch(url)
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.log(err))
+.finally(() => console.log("Data fetched with then chanining successfully"))
+
 // async/await ilə datanı fetch edin
+
+
+const fetchData = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    
+    console.log(data)
+}
+
+fetchData()
--- js/interval-timeout/script.js
+++ js/interval-timeout/script.js
@@ -3,33 +3,33 @@ const backBtn = document.querySelector("#back");
 const forwardBtn = document.querySelector("#forward");
 const timeout = document.querySelector("p");
 
-let intervalId;
+// let intervalId;
 
-const createMessage = (seconds) => {
-  return `Calculating... ${seconds} seconds remaining`;
-};
+// const createMessage = (seconds) => {
+//   return `Calculating... ${seconds} seconds remaining`;
+// };
 
-const createSizes = () => {
-  return `Window size: ${window.innerWidth} x ${window.innerHeight}`;
-};
+// const createSizes = () => {
+//   return `Window size: ${window.innerWidth} x ${window.innerHeight}`;
+// };
 
-button.addEventListener("click", () => {
-  button.disabled = true;
-  let count = 3;
+// button.addEventListener("click", () => {
+//   button.disabled = true;
+//   let count = 3;
 
-  timeout.textContent = createMessage(count);
+//   timeout.textContent = createMessage(count);
 
-  intervalId = setInterval(() => {
-    count--;
-    if (count > 0) {
-      timeout.textContent = createMessage(count);
-    } else {
-      clearInterval(intervalId);
-      timeout.textContent = `Calculation complete. ${createSizes()}. (Click button to calculate again)`;
-      button.disabled = false;
-    }
-  }, 1000);
-});
+//   intervalId = setInterval(() => {
+//     count--;
+//     if (count > 0) {
+//       timeout.textContent = createMessage(count);
+//     } else {
+//       clearInterval(intervalId);
+//       timeout.textContent = `Calculation complete. ${createSizes()}. (Click button to calculate again)`;
+//       button.disabled = false;
+//     }
+//   }, 1000);
+// });
 
 backBtn.addEventListener("click", () => {
   console.log(window.history);
@@ -48,3 +48,23 @@ forwardBtn.addEventListener("click", () => {
     alert("No next page in history.");
   }
 });
+
+const changeTimeoutCount = (count) => {
+  timeout.innerHTML = `Calculating... ${count} seconds remaining.`
+}
+
+button.addEventListener('click', () => {
+  let count = 3;
+  changeTimeoutCount(count)
+  button.disabled = true;
+
+  const intervalId =  setInterval(() => {
+    count -= 1;
+    changeTimeoutCount(count)
+    if (count == 0){
+      clearInterval(intervalId)
+      timeout.innerHTML = `Calculation completed. Window size: ${window.innerWidth} x ${window.innerHeight}`
+      button.disabled = false;
+    }
+  }, 1000)
+})
