f353fa375a4e92b46750f91c802d7cf83f4f979f
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-07-08 17:07:58 +0400

    async/await & interval/timeout

--- js/async/script.js
+++ js/async/script.js
@@ -2,20 +2,21 @@ const url = "https://fakestoreapi.com/products";
 
 // then chaining ilə datanı fetch edin
 
-fetch(url)
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => console.log(err))
-.finally(() => console.log("Data fetched with then chanining successfully"))
+// fetch(url)
+// .then(res => res.json())
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+// .finally(() => console.log("Data fetched with then chanining successfully"))
 
 // async/await ilə datanı fetch edin
 
 
-const fetchData = async () => {
-    const response = await fetch(url)
-    const data = await response.json()
+// const fetchData = async () => {
+//     const response = await fetch(url)
+//     const data = await response.json()
     
-    console.log(data)
-}
+//     console.log(data)
+// }
 
-fetchData()
+// fetchData()
+
